8d1d075c2ede066afb025abff3648d01f2742010
shalinipriya10 <shalinipriya10.11@gmail.com>
2022-12-12 23:06:56 +0530

    update

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var txtinput = document.querySelector("#txt-input")
 console.log(txtinput)
 btnTranslate.addEventListener("click",function clickEventHandler(){
     console.log("clicked")
-    console.log("input",txtInput.value)
+    console.log("input",txtinput.value)
 })
