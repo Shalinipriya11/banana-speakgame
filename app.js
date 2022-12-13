7df96af7bd65608591fa2240f52f79b41cfb258c
shalinipriya10 <shalinipriya10.11@gmail.com>
2022-12-13 11:22:31 +0530

    input-output processing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var btnTranslate = document.querySelector("#btn-Translate")
 var txtinput = document.querySelector("#txt-input")
-console.log(txtinput)
-btnTranslate.addEventListener("click",function clickEventHandler(){
-    console.log("clicked")
-    console.log("input",txtinput.value)
-})
+var outputDiv = document.querySelector("#output");
+
+function clickHandler(){
+ outputDiv.innerText = "ahhhdhdhdhd  "  + txtinput.value;
+}
+btnTranslate.addEventListener("click",clickHandler)
